79cceed8a280cd08a045c12b8a8cbb54d19d96d7
bboss <bsboss@neiu.edu>
2020-05-03 20:14:18 -0500

    test bootstrap and files

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$page_title = "Brandon Boss";
+include "includes/header.html";
+?>
+<body>
+    <a type="button" class="btn btn-primary" href="resources/Brandon%20Boss%20-%20April%202020%20-%20CS.txt">TXT</a>
+    <a type="button" class="btn btn-primary" href="resources/Brandon%20Boss%20-%20April%202020%20-%20CS.pdf">PDF</a>
+</body>
+</html>
+
+<?php include "includes/footer.html" ?>
